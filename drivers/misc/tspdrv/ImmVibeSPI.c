missesmr/android_kernel_xiaomi_armani_DEPRECIATED

misc: ImmVibeSPI: add support for setting vibration intensity

drivers/misc/tspdrv/ImmVibeSPI.c
@@ -49,6 +49,8 @@
 #define GPIO_HAPTIC_EN 50
 #define ISA1000_VIB_DEFAULT_TIMEOUT	15000
 
+static int pwm_duty = 0;
+
 /*
 ** PWM to ISA1000
 */
@@ -85,7 +87,7 @@ static int isa1000_vib_set(struct isa1000_vib *vib, int on)
 
 	if (on) {
 		rc = pwm_config(pwm,
-						(period_us * 80/100),
+						(period_us * pwm_duty) / 100,
 						period_us);
 		if (rc < 0){
 			printk( "Unable to config pwm\n");
@@ -213,6 +215,30 @@ static void isa1000_vib_set_level(int level)
 chip_dwn:
         gpio_set_value_cansleep(GPIO_ISA1000_EN, 0);
 }
+
+static ssize_t vibrator_amp_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", pwm_duty);
+}
+
+static ssize_t vibrator_amp_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+    int gain;
+	sscanf(buf, "%d", &gain);
+
+    if(gain>100)
+        gain=100;
+    else if(gain<80)
+        gain=80;
+
+    pwm_duty = gain;
+
+	return size;
+}
+
+static DEVICE_ATTR(amp, S_IRUGO | S_IWUSR, vibrator_amp_show, vibrator_amp_store);
 
 static int isa1000_setup(void)
 {
@@ -355,6 +381,11 @@ VibeStatus ImmVibeSPI_ForceOut_Initialize(void)
         return VIBE_E_FAIL;
     }
     /**/
+
+    Ret = device_create_file(vib_dev->timed_dev.dev, &dev_attr_amp);
+    if (Ret < 0) {
+	pr_err("[VIB] %s, create sysfs fail: amp\n", __func__);
+    }
 
     /* Disable amp */
     ImmVibeSPI_ForceOut_AmpDisable(0);

sinisa94/android_kernel_xiaomi_dior

drivers/misc/tspdrv/ImmVibeSPI.c
@@ -51,6 +51,8 @@
 #define GPIO_HAPTIC_EN 51
 #define ISA1000_VIB_DEFAULT_TIMEOUT	15000
 
+static int pwm_duty = 0;
+
 /*
 ** PWM to ISA1000
 */
@@ -86,7 +88,7 @@ static int isa1000_vib_set(struct isa1000_vib *vib, int on)
 
 	if (on) {
 		rc = pwm_config(pwm,
-						(pwm_period_ns * 100/100),
+						(pwm_period_ns * pwm_duty) / 100,
 						pwm_period_ns);
 		if (rc < 0){
 			pr_err( "Unable to config pwm%d\n",rc);
@@ -213,6 +215,30 @@ static void isa1000_vib_set_level(int level)
 chip_dwn:
         gpio_set_value_cansleep(GPIO_ISA1000_EN, 0);
 }
+
+static ssize_t vibrator_amp_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", pwm_duty);
+}
+
+static ssize_t vibrator_amp_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+    int gain;
+	sscanf(buf, "%d", &gain);
+
+    if(gain>100)
+        gain=100;
+    else if(gain<80)
+        gain=80;
+
+    pwm_duty = gain;
+
+	return size;
+}
+
+static DEVICE_ATTR(amp, S_IRUGO | S_IWUSR, vibrator_amp_show, vibrator_amp_store);
 
 static int isa1000_setup(void)
 {
@@ -366,6 +392,11 @@ VibeStatus ImmVibeSPI_ForceOut_Initialize(void)
         return VIBE_E_FAIL;
     }
     /**/
+
+    Ret = device_create_file(vib_dev->timed_dev.dev, &dev_attr_amp);
+    if (Ret < 0) {
+	pr_err("[VIB] %s, create sysfs fail: amp\n", __func__);
+    }
 
     /* Disable amp */
     ImmVibeSPI_ForceOut_AmpDisable(0);